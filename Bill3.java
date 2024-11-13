ba48d96819136482776aba383289584cb9360cdc
rotem80 <rrolev18@gmail.com>
2024-11-13 04:39:59 -0800

    check 3

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,8 @@ public class Bill3 {
 		String name2 = args [1];
 		String name3 = args [2];
 		int value = Integer.parseInt(args[3]);
-		double BillForEach =  Math.ceil (value / 3.0);
+		double split = (double) value;
+		double BillForEach =  Math.ceil (split / 3.0);
 		System.out.println("Dear " + name1 +", "+ name2+ ", and "+ name3+ ": Pay " + BillForEach + " Shekels each" );
 	
 	}
